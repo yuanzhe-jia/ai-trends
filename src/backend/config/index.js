2761bf5742e08a4777906e351c8e881d53a8423f
yuanzhe-jia <yuanzhe.dev@gmail.com>
2026-06-25 12:57:54 +0800

    fix: remove invalid RSS feeds (jiqizhixin, csdn, zhihu)

--- src/backend/config/index.js
+++ src/backend/config/index.js
@@ -13,13 +13,10 @@ const config = {
     { name: '掘金', url: 'https://juejin.cn/rss' },
     { name: '36氪', url: 'https://36kr.com/feed' },
     { name: '钛媒体', url: 'https://www.tmtpost.com/rss' },
-    { name: '机器之心', url: 'https://www.jiqizhixin.com/rss' },
-    { name: 'CSDN', url: 'https://blog.csdn.net/rss' },
     { name: 'OSCHINA', url: 'https://www.oschina.net/news/rss' },
     { name: '雷峰网', url: 'https://www.leiphone.com/feed' },
     { name: '爱范儿', url: 'https://www.ifanr.com/feed' },
     { name: 'IT之家', url: 'https://www.ithome.com/rss/' },
-    { name: '知乎精选', url: 'https://www.zhihu.com/rss' },
     { name: '少数派', url: 'https://sspai.com/feed' },
   ],
   
